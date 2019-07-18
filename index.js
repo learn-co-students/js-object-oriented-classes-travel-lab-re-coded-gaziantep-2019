6810cdf9faec460022f516b4d6dda2e1d291153e
Ammar Sammour <ammar.n.sammour@gmail.com>
2019-07-18 13:03:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-
+class Driver{
+  constructor(name, startDate){
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
+  yearsExperienceFromBeginningOf(year){
+    return year - this.startDate.getFullYear();
+  }
+}
+class Route {
+  constructor(beginingLocation, endingLocation) {
+    this.beginingLocation = beginingLocation;
+    this.endingLocation   = endingLocation;
+  }
+  blocksTravelled() {
+        let eastWest = [
+      '1st Avenue',
+      '2nd Avenue',
+      '3rd Avenue',
+      'Lexington Avenue',
+      'Park',
+      'Madison Avenue',
+      '5th Avenue'
+    ];
+    return (this.endingLocation.vertical  - this.beginingLocation.vertical) + eastWest.indexOf(this.endingLocation.horizontal) - eastWest.indexOf(this.beginingLocation.horizontal);
+  }
+  estimatedTime(peakHours) {
+    if(peakHours){
+      return this.blocksTravelled() / 2;
+    }
+    return this.blocksTravelled() / 3;
+  }
+}
