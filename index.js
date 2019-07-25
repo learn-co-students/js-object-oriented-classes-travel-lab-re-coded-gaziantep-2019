ae40fea1c01825b0169f3e8ef864c501d7222ac4
Ali Mert Uytun <uytunmert801@gmail.com>
2019-07-25 17:06:46 +0000

    solved

--- index.js
+++ index.js
@@ -1,1 +1,71 @@
+let today = new Date();
+let date1 = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+
+    let eastWest = [
+      '1st Avenue',
+      '2nd Avenue',
+      '3rd Avenue',
+      'Lexington Avenue',
+      'Park',
+      'Madison Avenue',
+      '5th Avenue'
+    ];
+    let northSouth = [
+      '1st Street',
+      '2nd Street',
+      '3rd Street',
+      '4th Street',
+      '5th Street',
+      '6th Street',
+      '7th Street'
+    ];
+
+class Route {
+  constructor( beginningLocation, endingLocation ) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+  avenueToInteger( avenue ) {
+    return eastWest.indexOf( avenue );
+  }
+  blocksTravelled() {
+    let horizontalDistance =
+      this.avenueToInteger( this.endingLocation.horizontal ) -
+      this.avenueToInteger( this.beginningLocation.horizontal );
+    let verticalDistance =
+      this.endingLocation.vertical - this.beginningLocation.vertical;
+    return Math.abs( horizontalDistance ) + Math.abs( verticalDistance );
+  }
+  estimatedTime( peak ) {
+    if ( peak ) {
+      return this.blocksTravelled() / 2;
+    } else {
+      return this.blocksTravelled() / 3;
+    }
+  }
+}
+
+
+class Driver {
+  constructor( name, startDate ) {
+    this.name = name;
+    this.startDate = new Date( startDate );
+  }
+  yearsExperienceFromBeginningOf( year ) {
+    let endDate = new Date( year, 1, 1 );
+    let totalYears = ( endDate - this.startDate ) / ( 365 * 24 * 60 * 60 * 1000 );
+    return parseInt( totalYears );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
 
